1b8e68fd04f43f8725afeae1c01018ee80ed1012
Tipparthi Divya sri <tipparthidivyasri@gmail.com>
2026-03-12 10:44:08 +0530

    text_preprocess.py

--- util/text_preprocess.py
+++ util/text_preprocess.py
@@ -12,21 +12,21 @@ stop_words = set(stopwords.words('english'))
 
 def clean_text(text):
 
-    # convert to lowercase
+    
     text = text.lower()
 
-    # remove numbers and symbols
+    
     text = re.sub(r"http\S+", "", text)
     text = re.sub(r"\S+@\S+", "", text)
     text = re.sub('[^a-z]', ' ', text)
 
-    # tokenize
+    
     words = text.split()
 
-    # remove stopwords
+    
     words = [w for w in words if w not in stop_words]
 
-    # stemming
+    
     words = [ps.stem(w) for w in words]
 
     return " ".join(words)
